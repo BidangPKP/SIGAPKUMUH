90e8fe2d96c1364d8bb8c1b95bda3468aca6dbd9
BidangPKP <nailahnafanil@gmail.com>
2025-11-12 10:25:01 +0700

    Revisi Pasca Uji Coba

---
+++ data/TitikKumuhRTLH2024_9.js
@@ -0,0 +1,10 @@
+var json_TitikKumuhRTLH2024_9 = {
+"type": "FeatureCollection",
+"name": "TitikKumuhRTLH2024_9",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "No": 23.0, "Nama": "Irianto", "Alamat": "Brawijaya 47, RT.004/RW.001", "Y": -7.09778, "X": 112.18611, "Desa": "Plaosan", "Kecamatan": "Babat", "Sebelum": null, "Sesudah": null }, "geometry": { "type": "Point", "coordinates": [ 112.18611111, -7.09777778 ] } },
+{ "type": "Feature", "properties": { "No": 24.0, "Nama": "Rais", "Alamat": "Jl. Udayana RT 001 RW 004", "Y": -7.09833, "X": 112.18667, "Desa": "Plaosan", "Kecamatan": "Babat", "Sebelum": null, "Sesudah": null }, "geometry": { "type": "Point", "coordinates": [ 112.18666667, -7.09833333 ] } },
+{ "type": "Feature", "properties": { "No": 25.0, "Nama": "Suwari", "Alamat": "Masjid RT 001 RW 004", "Y": -7.09806, "X": 112.18611, "Desa": "Plaosan", "Kecamatan": "Babat", "Sebelum": null, "Sesudah": null }, "geometry": { "type": "Point", "coordinates": [ 112.18611111, -7.09805556 ] } }
+]
+}
---
+++ data/TitikKumuhRTLH2025_8.js
@@ -0,0 +1,9 @@
+var json_TitikKumuhRTLH2025_8 = {
+"type": "FeatureCollection",
+"name": "TitikKumuhRTLH2025_8",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "No": 29.0, "Nama": "Edi Susanto", "KTP": "3524222808690001", "Jenis Kela": "L", "Alamat": "Dsn. Kaotan RT 004 RW 001 Ds. Sumberejo", "Desa/Kelur": "Sumberejo", "Kecamatan": "Lamongan", "Tempat": "Lamongan", "Tanggal La": "28 Agustus 1969", "Usia": "56", "Nama Ibu": "Rateni", "Sebelum": "D:\\QGIS\\Dokumentasi\\RTLH 2025\\29 0.jpg", "Sesudah": "D:\\QGIS\\Dokumentasi\\RTLH 2025\\29 100.jpg" }, "geometry": { "type": "Point", "coordinates": [ 112.38555556, -7.13361111 ] } },
+{ "type": "Feature", "properties": { "No": 30.0, "Nama": "Kasiati", "KTP": "3524225112690004", "Jenis Kela": "P", "Alamat": "RT 005 RW 001 Ds. Wajik", "Desa/Kelur": "Wajik", "Kecamatan": "Lamongan", "Tempat": "Lamongan", "Tanggal La": "12 November 1969", "Usia": "56", "Nama Ibu": "Lepok", "Sebelum": "D:/QGIS/Dokumentasi/RTLH 2025/30 0.jpg", "Sesudah": "D:\\QGIS\\Dokumentasi\\RTLH 2025\\30 100.jpg" }, "geometry": { "type": "Point", "coordinates": [ 112.39694444, -7.14333333 ] } }
+]
+}
